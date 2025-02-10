0e4da04ead8bea3f2043ce6a420a86b67b455e8a
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-02-10 18:27:28 +0900

    refactor: target 上げ + 不要オプション消し

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'node:path';
+// import path from 'node:path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -6,33 +6,34 @@ import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 
-import { getFileList } from './tools/get_file_list';
+// import { getFileList } from './tools/get_file_list';
 
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
+// const publicDir = path.resolve(__dirname, './public');
+// const getPublicFileList = async (targetPath: string) => {
+//   const filePaths = await getFileList(targetPath);
+//   const publicFiles = filePaths
+//     .map((filePath) => path.relative(publicDir, filePath))
+//     .map((filePath) => path.join('/', filePath));
 
-  return publicFiles;
-};
+//   return publicFiles;
+// };
 
 export default defineConfig(async () => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
+  // const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
 
   return {
     build: {
-      assetsInlineLimit: 20480,
-      cssCodeSplit: false,
-      cssTarget: 'es6',
-      minify: false,
-      rollupOptions: {
-        output: {
-          experimentalMinChunkSize: 40960,
-        },
-      },
-      target: 'es2015',
+      // assetsInlineLimit: 20480,
+      // cssCodeSplit: false,
+      // cssTarget: 'es6',
+      // minify: false,
+      // rollupOptions: {
+      //   output: {
+      //     experimentalMinChunkSize: 40960,
+      //   },
+      // },
+      // target: 'es2015',
+      target: "esnext"
     },
     plugins: [
       react(),
@@ -41,7 +42,7 @@ export default defineConfig(async () => {
       ViteEjsPlugin({
         module: '/src/client/index.tsx',
         title: '買えるオーガニック',
-        videos,
+        // videos,
       }),
     ],
   };

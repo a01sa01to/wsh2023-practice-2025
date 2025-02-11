edd779514159809b4a146e398059b1b0f904be0e
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-02-11 12:11:12 +0900

    fix: target が esnext になったので  top level await が使える

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import visualizer from "rollup-plugin-visualizer"
 import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 
 export default defineConfig(async () => {
@@ -14,7 +13,6 @@ export default defineConfig(async () => {
       visualizer(),
       react(),
       wasm(),
-      topLevelAwait(),
       ViteEjsPlugin({
         module: '/src/client/index.tsx',
         title: '買えるオーガニック',

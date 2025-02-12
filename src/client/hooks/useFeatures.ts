285a8db190127d377a19cdfe53eb53a86bee3bda
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-02-12 20:12:28 +0900

    feat: suspenseQuery をやめる

--- src/client/hooks/useFeatures.ts
+++ src/client/hooks/useFeatures.ts
@@ -1,12 +1,12 @@
-import { useSuspenseQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 import type { GetFeatureSectionsQueryResponse } from '../graphql/queries';
 import { GetFeatureSectionsQuery } from '../graphql/queries';
 
 export const useFeatures = () => {
-  const featuresResult = useSuspenseQuery<GetFeatureSectionsQueryResponse>(GetFeatureSectionsQuery);
+  const featuresResult = useQuery<GetFeatureSectionsQueryResponse>(GetFeatureSectionsQuery);
 
-  const features = featuresResult.data?.features;
+  const features = featuresResult?.data?.features;
 
   return { features };
 };

4e1c6db4a59e83c6cd440881c244409c0c55360e
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-02-15 16:53:29 +0900

    feat: 詳細読み込まれていない時点での CLS 対策

--- src/client/components/product/ProductMediaListPreviewer/MediaItemPreviewer/MediaItemPreviewer.tsx
+++ src/client/components/product/ProductMediaListPreviewer/MediaItemPreviewer/MediaItemPreviewer.tsx
@@ -7,15 +7,17 @@ import { Image } from '../../../foundation/Image';
 import styles from './MediaItemPreiewer.module.css';
 
 type Props = {
-  file: MediaFileFragmentResponse;
+  file?: MediaFileFragmentResponse;
 };
 
 export const MediaItemPreviewer: FC<Props> = ({ file }) => {
+  if (!file) return null
+
   const type = getMediaType(file.filename);
 
   return (
     <div className={styles.container}>
-      {type === 'image' && <Image fill src={file.filename} />}
+      {type === 'image' && <Image fill decoding='sync' loading='eager' src={file.filename} />}
       {type === 'video' && <video autoPlay controls muted playsInline className={styles.video} src={file.filename} />}
     </div>
   );

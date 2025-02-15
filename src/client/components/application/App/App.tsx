8d880871bbe02b3fff844e0eeaad2a098e03e3c8
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-02-15 19:51:15 +0900

    feat: root style を `index.html` に直書き

--- src/client/components/application/App/App.tsx
+++ src/client/components/application/App/App.tsx
@@ -1,20 +1,12 @@
 import type { FC } from 'react';
-import { Helmet } from 'react-helmet';
 
 import { SignInModal } from '../../modal/SignInModal';
 import { SignUpModal } from '../../modal/SignUpModal';
 import { Providers } from '../Providers';
 import { Routes } from '../Routes';
 
-import resetStyle from 'modern-css-reset/src/reset.css?raw';
-import rootStyle from './root.css?raw';
-
 export const App: FC = () => (
   <Providers>
-    <Helmet>
-      <style>{resetStyle}</style>
-      <style>{rootStyle}</style>
-    </Helmet>
     <Routes />
     <SignInModal />
     <SignUpModal />

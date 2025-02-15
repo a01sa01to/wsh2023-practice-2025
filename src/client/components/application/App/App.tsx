fc37942742df88752cd0af37a11dc3c0bebe64d8
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-02-15 17:35:35 +0900

    feat: reset, root style を inline

--- src/client/components/application/App/App.tsx
+++ src/client/components/application/App/App.tsx
@@ -1,15 +1,21 @@
 import type { FC } from 'react';
+import { Helmet } from 'react-helmet';
 
 import { SignInModal } from '../../modal/SignInModal';
 import { SignUpModal } from '../../modal/SignUpModal';
 import { Providers } from '../Providers';
 import { Routes } from '../Routes';
 
-import 'modern-css-reset/src/reset.css';
-import './root.css';
+
+import resetStyle from 'modern-css-reset/src/reset.css?raw';
+import rootStyle from './root.css?raw';
 
 export const App: FC = () => (
   <Providers>
+    <Helmet>
+      <style>{resetStyle}</style>
+      <style>{rootStyle}</style>
+    </Helmet>
     <Routes />
     <SignInModal />
     <SignUpModal />
